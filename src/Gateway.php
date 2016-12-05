c2130a843352743d39b8baedce7d866437ab4b0f
Radu <radu.bordei89@gmail.com>
2016-12-05 21:53:48 +0200

    initial files.

---
+++ src/Gateway.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Parfumix\TwoCheckout;
+
+use Omnipay\Common\AbstractGateway;
+use Parfumix\TwoCheckout\Message\CompletePurchaseRequest;
+use Parfumix\TwoCheckout\Message\PurchaseRequest;
+
+class Gateway extends AbstractGateway {
+
+	/**
+	 * Get gateway display name
+	 *
+	 * This can be used by carts to get the display name for each gateway.
+	 */
+	public function getName() {
+		// TODO: Implement getName() method.
+	}
+
+	/**
+	 * Get gateway default parameters .
+	 *
+	 * @return array
+	 */
+	public function getDefaultParameters() {
+		return array(
+			'accountNumber' => null,
+			'secretWord' => null,
+			// if true, transaction with the live checkout URL will be a demo sale and card won't be charged.
+			'testMode' => false,
+		);
+	}
+
+	/**
+	 * Implement purchase request method .
+	 *
+	 * @param array $options
+	 * @return \Omnipay\Common\Message\AbstractRequest
+	 */
+	public function purchase(array $options = array()) {
+		return $this->createRequest(PurchaseRequest::class, $options);
+	}
+
+	/**
+	 * Complete purchase request .
+	 *
+	 * @param array $options
+	 * @return \Omnipay\Common\Message\AbstractRequest
+	 */
+	public function completePurchase(array $options = array()) {
+		return $this->createRequest(CompletePurchaseRequest::class, $options);
+	}
+
+}
---
+++ src/Message/PurchaseResponse.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Parfumix\TwoCheckout\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RedirectResponseInterface;
+
+class PurchaseResponse extends AbstractResponse
+	implements RedirectResponseInterface {
+
+	protected $liveEndpoint = 'https://www.2checkout.com/checkout/purchase';
+	protected $testEndpoint = 'https://sandbox.2checkout.com/checkout/purchase';
+
+	/**
+	 * Get appropriate 2checkout endpoints.
+	 *
+	 * @return string
+	 */
+	public function getEndPoint() {
+		$endpoint = $this->liveEndpoint;
+
+		if (isset($this->data['sandbox']) && $this->data['sandbox'] === true)
+			$endpoint = $this->testEndpoint;
+
+		return $endpoint;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isSuccessful() {
+		return false;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isRedirect() {
+		return true;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRedirectUrl() {
+		$endpoint = $this->getEndPoint();
+
+		// remove the sandbox parameter.
+		unset($this->data['sandbox']);
+
+		$url = $endpoint . '?' . http_build_query($this->data);
+
+		// Fix for some sites that encode the entities
+		return str_replace('&amp;', '&', $url);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRedirectMethod() {
+		return 'GET';
+	}
+
+	/**
+	 * No redirect data.
+	 */
+	public function getRedirectData() {
+		return;
+	}
+}
