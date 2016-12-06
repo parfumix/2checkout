14f97d9af5af0562c3ee989b0faea5616549dfa2
Radu <radu.bordei89@gmail.com>
2016-12-06 23:29:06 +0200

    validate response.

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -15,25 +15,21 @@ class CompletePurchaseRequest extends PurchaseRequest {
 	public function getData() {
 		// if 2co didn't send a POST body parameters, use sent GET string parameters instead.
 		// Note: when redirect is set to Header redirect in 2co dashboard, transaction parameters are GET query string.
-		$fetchPostBody = $this->httpRequest->request->all();
-		if (empty($fetchPostBody)) {
-			$request_type = 'query';
-		} else {
-			$request_type = 'request';
-		}
+		$request_type = empty($this->httpRequest->request->all())
+			? 'query'
+			: 'request';
 
-		$orderNo = $this->httpRequest->$request_type->get('order_number');
+		$orderNo     = $this->httpRequest->$request_type->get('order_number');
 		$orderAmount = $this->httpRequest->$request_type->get('total');
 
 		// strange exception specified by 2Checkout
-		if ( $this->getTestMode() ) {
+		if ( $this->getTestMode() )
 			$orderNo = '1';
-		}
 
-		$key = md5($this->getSecretWord() . $this->getAccountNumber() . $orderNo . $orderAmount);
-		if (strtolower($this->httpRequest->$request_type->get('key')) !== $key) {
+		$key = strtoupper(md5($this->getSecretWord() . $this->getSid() . $orderNo . $orderAmount));
+
+		if ($this->httpRequest->$request_type->get('key') !== $key)
 			throw new InvalidResponseException('Invalid key');
-		}
 
 		return $this->httpRequest->$request_type->all();
 	}
